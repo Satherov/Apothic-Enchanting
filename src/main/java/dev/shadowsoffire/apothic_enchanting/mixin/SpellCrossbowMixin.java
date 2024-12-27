1b7c2a3591b0e3633a5babfad72ec2c7fc1d3723
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-12-26 19:09:12 -0800

    Fix ars spell crossbow mixin

--- src/main/java/dev/shadowsoffire/apothic_enchanting/mixin/SpellCrossbowMixin.java
+++ src/main/java/dev/shadowsoffire/apothic_enchanting/mixin/SpellCrossbowMixin.java
@@ -2,30 +2,36 @@ package dev.shadowsoffire.apothic_enchanting.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.world.item.CrossbowItem;
+import dev.shadowsoffire.apothic_enchanting.enchantments.CrescendoHooks;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 @Pseudo
 @Mixin(targets = "com.hollingsworth.arsnouveau.common.items.SpellCrossbow", remap = false)
-public class SpellCrossbowMixin extends CrossbowItem {
+public class SpellCrossbowMixin {
 
-    public SpellCrossbowMixin(Properties pProperties) {
-        super(pProperties);
+    /**
+     * The spell crossbow has a different implementation of this method, so it needs a separate injection. Underlying logic should be the same.
+     */
+    @Inject(method = "Lcom/hollingsworth/arsnouveau/common/items/SpellCrossbow;tryLoadProjectiles(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)Z", at = @At(value = "RETURN"))
+    private static void apoth_setupCrescendoShots(LivingEntity shooter, ItemStack crossbow, CallbackInfoReturnable<Boolean> cir) {
+        if (!shooter.level().isClientSide()) {
+            CrescendoHooks.prepareCrescendoShots(shooter, crossbow);
+        }
     }
 
-//    @Inject(method = "m_7203_", at = @At(value = "INVOKE", target = "Lcom/hollingsworth/arsnouveau/common/items/SpellCrossbow;shootStoredProjectiles(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;FF)V", remap = false))
-//    public void apoth_preFired(Level pLevel, Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> ci) {
-//        CrescendoEnchant.preArrowFired(pPlayer.getItemInHand(pHand));
-//    }
-//
-//    @Inject(method = "m_7203_", at = @At(value = "RETURN", ordinal = 0))
-//    public void apoth_addCharges(Level pLevel, Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> ci) {
-//        CrescendoEnchant.onArrowFired(pPlayer.getItemInHand(pHand));
-//    }
-//
-//    @Inject(method = "getArrow", at = @At(value = "RETURN"), remap = false)
-//    private void apoth_markArrows(Level pLevel, LivingEntity pLivingEntity, ItemStack pCrossbowStack, ItemStack pAmmoStack, CallbackInfoReturnable<AbstractArrow> ci) {
-//        CrescendoHooks.markGeneratedArrows(ci.getReturnValue(), pCrossbowStack);
-//    }
+    /**
+     * Bailey writes very good code, so instead of overriding createProjectile, there's a separate private method that needs to be hooked into.
+     */
+    @Inject(method = "getArrow", at = @At(value = "RETURN"), require = 0)
+    private void apoth_markArrows(Level pLevel, LivingEntity pLivingEntity, ItemStack pCrossbowStack, ItemStack pAmmoStack, CallbackInfoReturnable<Projectile> cir) {
+        CrescendoHooks.markGeneratedArrows(cir.getReturnValue(), pCrossbowStack);
+    }
 
 }
