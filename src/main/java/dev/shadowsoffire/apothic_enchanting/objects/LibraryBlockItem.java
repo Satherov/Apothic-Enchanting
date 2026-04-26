17fae578d621af61d8899ad689f7c8ee45d949ff
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2026-04-26 17:40:33 -0400

    Fix library of alexandria losing enchantments when upgraded and placed

--- src/main/java/dev/shadowsoffire/apothic_enchanting/objects/LibraryBlockItem.java
+++ src/main/java/dev/shadowsoffire/apothic_enchanting/objects/LibraryBlockItem.java
@@ -2,17 +2,25 @@ package dev.shadowsoffire.apothic_enchanting.objects;
 
 import java.util.function.Consumer;
 
+import org.jspecify.annotations.Nullable;
+
+import dev.shadowsoffire.apothic_enchanting.library.EnchLibraryBlock;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.component.TypedEntityData;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
 /**
  * BlockItem for the enchantment library blocks that surfaces capacity + stored point count in
@@ -36,6 +44,39 @@ public class LibraryBlockItem extends BlockItem {
             if (points > 0) {
                 tooltip.accept(Component.translatable("tooltip.enchlib.item", points).withStyle(ChatFormatting.GRAY));
             }
+        }
+    }
+
+    @Override
+    protected boolean updateCustomBlockEntityTag(BlockPos pos, Level level, @Nullable Player player, ItemStack itemStack, BlockState placedState) {
+        return updateLibraryBETag(level, player, pos, itemStack);
+    }
+
+    /**
+     * Variant of {@link #updateCustomBlockEntityTag(Level, Player, BlockPos, ItemStack)} which has a more lenient check than type != customData.type()
+     */
+    private boolean updateLibraryBETag(Level level, @Nullable Player player, BlockPos pos, ItemStack itemStack) {
+        if (level.isClientSide()) {
+            return false;
+        }
+        else {
+            TypedEntityData<BlockEntityType<?>> customData = itemStack.get(DataComponents.BLOCK_ENTITY_DATA);
+            if (customData != null) {
+                BlockEntity blockEntity = level.getBlockEntity(pos);
+                if (blockEntity != null) {
+                    if (!customData.type().getValidBlocks().stream().anyMatch(EnchLibraryBlock.class::isInstance)) {
+                        return false;
+                    }
+
+                    if (player != null) {
+                        return customData.loadInto(blockEntity, level.registryAccess());
+                    }
+
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
